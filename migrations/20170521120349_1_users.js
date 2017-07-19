8b085e4a72e0c25046bcf7e5c2f7710a8227508c
rhysgoehring <rhysgoehring@gmail.com>
2017-07-19 11:31:45 -0600

    added lat and lng to events table and added an about column to the users table

--- migrations/20170521120349_1_users.js
+++ migrations/20170521120349_1_users.js
@@ -6,6 +6,7 @@ exports.up = function(knex) {
     table.string('first_name', 255).notNullable();
     table.string('last_name', 255).notNullable();
     table.text('email').unique().notNullable();
+    table.string('about', 255);
     table.specificType('hashed_password', 'char(60)').notNullable();
     table.integer('zip').notNullable();
     table.string('profile_pic').defaultTo('https://www.universetoday.com/wp-content/uploads/2009/12/galileo-e1435358932718.jpg')
--- migrations/20170521122217_3_events.js
+++ migrations/20170521122217_3_events.js
@@ -6,6 +6,8 @@ exports.up = function(knex) {
     table.string('name', 255).notNullable();
     table.integer('cat_id').references('id').inTable('categories').notNullable().onDelete('CASCADE');
     table.string('location').notNullable();
+    table.decimal('lat', 9,6);
+    table.decimal('lng', 9,6);
     table.string('description').notNullable();
     table.string('event_date').notNullable();
     table.integer('owner_id').references('id').inTable('users').notNullable().onDelete('CASCADE');
